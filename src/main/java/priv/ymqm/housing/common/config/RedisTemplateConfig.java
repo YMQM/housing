8336ce8aa5774560ea780d695a36c0dd1161f768
chenhn <honnianchen@qq.com>
2020-03-22 14:46:28 +0800

    Add redis template config

---
+++ src/main/java/priv/ymqm/housing/common/config/RedisTemplateConfig.java
@@ -0,0 +1,22 @@
+package priv.ymqm.housing.common.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.data.redis.connection.RedisConnectionFactory;
+import org.springframework.data.redis.core.RedisTemplate;
+import org.springframework.data.redis.serializer.StringRedisSerializer;
+
+/**
+ * @author chenhonnian
+ * @since 2020/03/21
+ */
+@Configuration
+public class RedisTemplateConfig {
+    @Bean
+    public RedisTemplate<Object, Object> redisTemplate(RedisConnectionFactory redisConnectionFactory) {
+        RedisTemplate<Object, Object> redisTemplate = new RedisTemplate<>();
+        redisTemplate.setConnectionFactory(redisConnectionFactory);
+        redisTemplate.setKeySerializer(new StringRedisSerializer());
+        return redisTemplate;
+    }
+}
